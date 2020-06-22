821c01deca9e33020ad772b12d4b3039c1ccba89
unknown <dmcdan06@gmail.com>
2020-06-22 17:59:07 -0400

    Added notes for changes that need made

--- GroceryPrices.py
+++ GroceryPrices.py
@@ -1,10 +1,15 @@
+# Add ability to search Kroger for items
+
 grocery_list = []
 
+# Function to add items to the grocery list.
 def grocery_add(item):
     grocery_list.append(item)
     print("The list now has {} items".format(len(grocery_list)))
 
+# Add instructions
 
+# While loop to add grocery items to the list until finished.
 while True:
     new_item =input("> ")
     if new_item == "DONE":
@@ -12,6 +17,14 @@ while True:
     grocery_add(new_item)
 
 
+# Printed out grocery list
 print("Grocery List:")
 for food_item in grocery_list:
     print(food_item)
+
+
+# Create a function that adds the total of all items together. 
+
+
+# Possibilities:
+# Add second API to search through to compare prices.
