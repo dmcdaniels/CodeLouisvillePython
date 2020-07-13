09d60645993643dc8ccffbbb794e60395c64545c
unknown <dmcdan06@gmail.com>
2020-07-13 19:22:45 -0400

    Updated GroceryPrices.py and created stores.py for food pricinG

--- GroceryPrices.py
+++ GroceryPrices.py
@@ -1,15 +1,24 @@
+import requests
+from stores.py import *
+
+
 # Add ability to search Kroger for items
+
+
+print(stores)
+
 
 grocery_list = []
 
-# Function to add items to the grocery list.
+#Function to add items to the grocery list.
 def grocery_add(item):
     grocery_list.append(item)
     print("The list now has {} items".format(len(grocery_list)))
 
-# Add instructions
+#Add instructions
 
-# While loop to add grocery items to the list until finished.
+
+#While loop to add grocery items to the list until finished.
 while True:
     new_item =input("> ")
     if new_item == "DONE":
@@ -17,14 +26,14 @@ while True:
     grocery_add(new_item)
 
 
-# Printed out grocery list
+#Printed out grocery list
 print("Grocery List:")
 for food_item in grocery_list:
     print(food_item)
 
 
-# Create a function that adds the total of all items together. 
+#Create a function that adds the total of all items together. 
 
 
-# Possibilities:
-# Add second API to search through to compare prices.
+#Possibilities:
+#Add second API to search through to compare prices.
---
+++ stores.py
@@ -0,0 +1,24 @@
+# Create some test data for our catalog in the form of a list of dictionaries.
+store1 = [
+    {'id': 0,
+     'item': 'Milk',
+     'cost': '$1.29',},
+    {'id': 1,
+     'item': 'Eggs',
+     'cost': '$1.59',},
+    {'id': 2,
+     'item': 'Bacon',
+     'cost': '$4.99',}
+]
+
+store2 = [
+     {'id': 0,
+     'item': 'Milk',
+     'cost': '$1.09',},
+    {'id': 1,
+     'item': 'Eggs',
+     'cost': '$1.19',},
+    {'id': 2,
+     'item': 'Bacon',
+     'cost': '$5.99',}
+]
