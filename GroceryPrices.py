8599c55302ba7dc49bbda770abb53b7664edabad
unknown <dmcdan06@gmail.com>
2020-07-27 18:18:11 -0400

    Cleaned up code, and comments.

--- GroceryPrices.py
+++ GroceryPrices.py
@@ -1,26 +1,23 @@
+# Add ability to search dictionary for store inventory. 
 from stores import store1
 from stores import store2
-# Add ability to search Kroger for items
 
-#Add instructions
-#make this a function
+# Add instructions
 def inventory():
     print ("These are the items we have available in our store:   ")
 
-print("Hello and welcome to our online shopping store. For our inventory please type 'inventory', when you are finished shopping type 'done'.")
+print("Hello and welcome to our online shopping store. For our inventory please type 'inventory', when you are finished shopping type 'done'. To add an item to the cart, just enter the name of the item")
 
-#Start with an empty grocery list.
+# Start with an empty grocery list.
 grocery_list = []
 
-#Function to add items to the grocery list.
+# Function to add items to the grocery list.
 def grocery_add(item, cost):
     grocery_list.append([item, cost])
     print("The list now has {} items".format(len(grocery_list)))
 
 
-
-
-#While loop to add grocery items, with their cost to the list until finished. If/elif functions for inventory and exiting the loop.
+# While loop to add grocery items, with their cost to the list until finished. If/elif functions for inventory and exiting the loop.
 while True:
     new_item =input("What would you like to purchase? ").lower()
     store_cost = store1.get(new_item)
