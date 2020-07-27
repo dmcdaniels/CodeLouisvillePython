946fb6917bea127b6cb783183517d1c0db0e8571
unknown <dmcdan06@gmail.com>
2020-07-27 14:46:58 -0400

    fixed inventory list

--- GroceryPrices.py
+++ GroceryPrices.py
@@ -5,7 +5,7 @@ from stores import store2
 #Add instructions
 #make this a function
 def inventory():
-    print ("These are the items we have available in our store")
+    print ("These are the items we have available in our store:   ")
 
 print("Hello and welcome to our online shopping store. For our inventory please type 'inventory', when you are finished shopping type 'done'.")
 
@@ -13,38 +13,28 @@ print("Hello and welcome to our online shopping store. For our inventory please 
 grocery_list = []
 
 #Function to add items to the grocery list.
-    #fix counting funtion when using inventory
-def grocery_add(item):
-    grocery_list.append(item)
+def grocery_add(item, cost):
+    grocery_list.append([item, cost])
     print("The list now has {} items".format(len(grocery_list)))
 
-#While loop to add grocery items, with their cost to the list until finished.
+
+
+
+#While loop to add grocery items, with their cost to the list until finished. If/elif functions for inventory and exiting the loop.
 while True:
-    new_item =input("> ").lower()
-    store_item = store1.get(new_item)
-    print (store_item)
+    new_item =input("What would you like to purchase? ").lower()
+    store_cost = store1.get(new_item)
     if new_item == "inventory":
         inventory()
         for item, cost in store1.items():
             print(item, ':', cost)
+        continue
     elif new_item == "done":
         break
-    grocery_add(new_item)
+    print (store_cost)
+    grocery_add(new_item, store_cost)
 
-
-#Printed out grocery list
-print("Grocery List:")
-for food_item, value in store1.items():
-    print(food_item, ':', value)
-
-
-#values = food_item.value()
-#total = sum(values)
-#print(total)
-
-
+#Print out grocery list
+print (grocery_list)
 
 #Create a function that adds the total of all items together. 
-#total = 0
-#for total_cost in new_item:
-#    total = total + new_item(total_cost)
